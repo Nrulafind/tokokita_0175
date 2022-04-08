a9cea24925b2e746322288422c448fc24fcb8a6b
Nrulafind <mnurula.nu246@gmail.com>
2022-04-08 15:23:03 +0700

    commit

---
+++ application/controllers/Adminpanel.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Adminpanel extends CI_Controller
+{
+    public function dashboard()
+    {
+        $this->template->load('layout_admin', 'admin/dashboard');
+    }
+    public function index()
+    {
+        $this->load->view('admin/form_login');
+    }
+}
