d1b031a7c4c078a0beb2b0a91f940547a2fad0f6
Nrulafind <mnurula.nu246@gmail.com>
2022-04-09 23:28:32 +0700

    commit

--- application/controllers/Member.php
+++ application/controllers/Member.php
@@ -17,6 +17,42 @@ class Member extends CI_Controller
     {
         $datawhere = array('idkurir' => $id);
         $data['kurir'] = $this->Mcrud->get_by_id('tbl_kurir', $datawhere)->row_object();
-        $this->template->load('layout_admin', 'admin/');
+        $this->template->load('layout_admin', 'admin/kurir/form_edit', $data);
+    }
+    public function status_member_aktifkan($id)
+    {
+        $datawhere = array('idkonsumen' => $id);
+        $data['member'] = $this->Mcrud->get_by_id('tbl_member', $datawhere)->row_object();
+        if ($data['member']->statusAktif == 'Y') {
+            # Error
+            $data['error'] = '<div class="alert alert-danger">Member Sudah Aktif</div>';
+            $data['member'] = $this->Mmember->join2table()->result();
+            $this->template->load('layout_admin', 'admin/member/index', $data);
+        } elseif ($data['member']->statusAktif == 'N') {
+            #Activate
+            $dataUpdate = array(
+                'statusAktif' => 'Y'
+            );
+            $this->Mmember->update_status('tbl_member', $dataUpdate, 'idkonsumen', $id);
+            redirect('member/index');
+        }
+    }
+    public function status_member_nonaktif($id)
+    {
+        $datawhere = array('idkonsumen' => $id);
+        $data['member'] = $this->Mcrud->get_by_id('tbl_member', $datawhere)->row_object();
+        if ($data['member']->statusAktif == 'N') {
+            #error
+            $data['error'] = '<div class="alert alert-danger">Member sudah NonAktif.</div>';
+            $data['member'] = $this->Mmember->join2table()->result();
+            $this->template->load('layout_admin', 'admin/member/index', $data);
+        } elseif ($data['member']->statusAktif == 'Y') {
+            # Activate
+            $dataUpdate = array(
+                'statusAktif' => 'N'
+            );
+            $this->Mmember->update_status('tbl_member', $dataUpdate, 'idkonsumen', $id);
+            redirect('member/index');
+        }
     }
 }
